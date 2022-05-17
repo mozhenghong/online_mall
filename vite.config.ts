80fdb01268ae9f7452dba3be97a00d3b5dc6c24a
momo <1445107381@qq.com>
2022-05-17 17:41:41 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,70 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import {
+  createStyleImportPlugin,
+  AndDesignVueResolve,
+  VantResolve,
+  ElementPlusResolve,
+  NutuiResolve,
+  AntdResolve,
+} from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    createStyleImportPlugin({
+      resolves: [
+        AndDesignVueResolve(),
+        VantResolve(),
+        ElementPlusResolve(),
+        NutuiResolve(),
+        AntdResolve(),
+      ],
+      libs: [
+        // 如果没有你需要的resolve，可以在lib内直接写，也可以给我们提供PR
+        {
+          libraryName: "ant-design-vue",
+          esModule: true,
+          resolveStyle: (name) => {
+            return `ant-design-vue/es/${name}/style/index`;
+          },
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    // 路径别名
+    alias: [
+      { find: "src", replacement: path.resolve(__dirname, "src") },
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+    ],
+  },
+  // less 配置
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // 更改主题
+          "primary-color": "#52c41a",
+          "link-color": "#1DA57A",
+          "border-radius-base": "2px",
+        },
+        javascriptEnabled: true,
+      },
+    },
+  },
+  // 本地开发配置
+  server: {
+    port: 8088,
+    proxy: {
+      "/api": {
+        target: "http://10.0.40.200:8979",
+        ws: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
